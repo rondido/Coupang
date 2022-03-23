66b915ab74cdc5a41fa4dd82b89058fecf615dde
Park <rppr001@naver.com>
2022-03-23 23:56:22 +0900

    2022-03-23 Router version 6 commit

---
+++ copuang-front-end/src/container/LoginContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LoginContainer = () => {
+    return (
+        <div>
+            {/* 로그인 관련 */}
+            <h1>로그인 컨테이너</h1>
+        </div>
+    );
+};
+
+export default LoginContainer;
---
+++ copuang-front-end/src/container/MainContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainContainer = () => {
+    return (
+       <>
+       {/* 메인 페이지 관련 코드 작성 */}
+       <h1>메인 컨테이너</h1>
+       </>
+    );
+};
+
+export default MainContainer;
